86d911dd5014762ec36198c0070126a1688733ff
Toke113 <114912578+Toke113@users.noreply.github.com>
2025-03-17 16:30:52 +0100

    Added a timernode and text node to customer

--- godot-game/Scripts/customer.gd
+++ godot-game/Scripts/customer.gd
@@ -4,3 +4,11 @@ var desired_music
 var timer
 
 var hint
+
+#TODO Function that sets the value of the customer
+
+func new_customer(desired_music, timer, hint):
+	desired_music = desired_music
+	timer = timer
+	hint = hint
+	pass
