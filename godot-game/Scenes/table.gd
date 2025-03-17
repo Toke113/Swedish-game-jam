b3a705e3ecf89f8bddbd2c2acb9e770d12df2f04
soniccobubu <sofia-006@ukr.net>
2025-03-17 18:03:24 +0200

    change

--- godot-game/Scenes/table.gd
+++ godot-game/Scenes/table.gd
@@ -1,5 +1,6 @@
 extends Node2D
-@onready var buttons_container = $HBoxContainer 
+
+@onready var buttons_container = $HBoxContainer
 @onready var all_buttons = [
 	$HBoxContainer/Button,
 	$HBoxContainer/Button2,
@@ -11,33 +12,23 @@ extends Node2D
 func _ready():
 	$ButtonLeft.pressed.connect(_move_left)
 	$ButtonRight.pressed.connect(_move_right)
-
+	_update_buttons()
 
 func _move_left():
-	var last_button = all_buttons.pop_back() 
-	all_buttons.push_front(last_button) 
-
+	all_buttons.append(all_buttons.pop_front()) # Сдвиг влево
 	_update_buttons()
-
 
 func _move_right():
-	var first_button = all_buttons.pop_front() 
-	all_buttons.push_back(first_button)  
-
+	all_buttons.push_front(all_buttons.pop_back()) # Сдвиг вправо
 	_update_buttons()
 
-
 func _update_buttons():
-	var last_button = all_buttons.pop_back()
-	all_buttons.push_front(last_button)
-	
-	for child in buttons_container.get_children():
-		buttons_container.remove_child(child)
-	
+	for btn in all_buttons:
+		btn.visible = false
+
 	for i in range(3):
 		var btn = all_buttons[i]
-		if btn and is_instance_valid(btn):
-			if btn.get_parent():
-				btn.get_parent().remove_child(btn)
+		btn.visible = true
+		if btn.get_parent() != buttons_container:
 			buttons_container.add_child(btn)
-			buttons_container.move_child(btn, i)
+		buttons_container.move_child(btn, i)
